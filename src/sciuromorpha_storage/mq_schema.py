e86c3ebcc2367dfc865c105fbf29f24876c9b74f
Whale Mo <ncwhale@gmail.com>
2023-12-10 09:32:32 +0000

    Add mq schema.

---
+++ src/sciuromorpha_storage/mq_schema.py
@@ -0,0 +1,33 @@
+from faststream.rabbit import RabbitExchange, RabbitQueue, ExchangeType
+
+# For meta rpc
+meta_rpc = RabbitExchange(
+    "meta-rpc", type=ExchangeType.DIRECT, durable=True, auto_delete=False
+)
+
+meta_topic = RabbitExchange(
+    "meta-topic", type=ExchangeType.TOPIC, durable=True, auto_delete=False
+)
+
+# For task rpc
+task_rpc = RabbitExchange(
+    "task-rpc", type=ExchangeType.DIRECT, durable=True, auto_delete=False
+)
+
+task_topic = RabbitExchange(
+    "task-topic", type=ExchangeType.TOPIC, durable=True, auto_delete=False
+)
+
+# For secret rpc
+secret_rpc = RabbitExchange(
+    "secret-rpc", type=ExchangeType.DIRECT, durable=True, auto_delete=False
+)
+
+# For storage rpc
+storage_rpc = RabbitExchange(
+    "storage-rpc", type=ExchangeType.DIRECT, durable=True, auto_delete=False
+)
+
+storage_topic = RabbitExchange(
+    "storage-topic", type=ExchangeType.TOPIC, durable=True, auto_delete=False
+)
